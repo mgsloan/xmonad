9a1aef3cf780c768d9322fd2d26691dcdfa903dc
Michael Sloan <mgsloan@gmail.com>
2019-01-06 16:29:59 -0800

    Allow proper process handling by handling sigCHLD

--- src/XMonad/Core.hs
+++ src/XMonad/Core.hs
@@ -794,7 +794,8 @@ trace = io . hPutStrLn stderr
 installSignalHandlers :: MonadIO m => m ()
 installSignalHandlers = io $ do
     installHandler openEndedPipe Ignore Nothing
-    installHandler sigCHLD Ignore Nothing
+    -- Note: mgsloan modification to allow for waiting for processes
+    -- installHandler sigCHLD Ignore Nothing
     (try :: IO a -> IO (Either SomeException a))
       $ fix $ \more -> do
         x <- getAnyProcessStatus False False
@@ -804,5 +805,6 @@ installSignalHandlers = io $ do
 uninstallSignalHandlers :: MonadIO m => m ()
 uninstallSignalHandlers = io $ do
     installHandler openEndedPipe Default Nothing
-    installHandler sigCHLD Default Nothing
+    -- Note: mgsloan modification to allow for waiting for processes
+    -- installHandler sigCHLD Default Nothing
     return ()
